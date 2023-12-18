a315653d3b85f76f57a5d4bbaeefe4822f57c63c
artvved <artvved2000@mail.ru>
2023-12-18 15:20:27 +0300

    vibrations

---
+++ Assets/Source/Scripts/System/Util/VibrationSystem.cs
@@ -0,0 +1,39 @@
+﻿using Kuhpik;
+using Leopotam.EcsLite;
+using MoreMountains.NiceVibrations;
+using Source.Scripts.Component.Event;
+
+namespace Source.Scripts.System.Util
+{
+    public class VibrationSystem : GameSystem
+    {
+        private EcsFilter filterDamage;
+        private EcsFilter filterDamageToPlayer;
+        
+        public override void OnInit()
+        {
+            base.OnInit();
+            filterDamage = eventWorld.Filter<DamageEvent>().End();
+            //filterDamageToPlayer = eventWorld.Filter<HitPlayerEvent>().End();
+        }
+
+        public override void OnUpdate()
+        {
+            base.OnUpdate();
+            foreach (var e in filterDamage)
+            {
+                Vibrate();
+                break;
+            }
+        }
+        
+        private void Vibrate()
+        {
+            if (!save.VibroOn)
+                return;
+
+            MMVibrationManager.Vibrate();
+          
+        }
+    }
+}
